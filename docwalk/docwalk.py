407c032b8a7e247b1c9260d680a2c4258b249468
Johan Hägg <kaustic@gmx.co.uk>
2014-05-02 19:57:11 +0200

    Copy-pasted as iterative depth-first traversal.

--- docwalk/docwalk.py
+++ docwalk/docwalk.py
@@ -7,14 +7,14 @@ class DocWalker:
             self._root = ast.parse(f.read(), filename=filename)
 
     def run(self):
-        return list(self._walk_breadth_first(self._root))
+        return list(self._walk_depth_first(self._root))
 
-    def _walk_depth_first(self, node, path=()):
+    def _walk_depth_first_recursive(self, node, path=()):
         for child in ast.iter_child_nodes(node):
             if isinstance(child, ast.ClassDef) or isinstance(child, ast.FunctionDef):
                 new_path = path + (child.name,)
                 yield ".".join(new_path)
-                for path_name in self._walk_depth_first(child, new_path):
+                for path_name in self._walk_depth_first_recursive(child, new_path):
                     yield path_name
 
     def _walk_breadth_first(self, root):
@@ -22,6 +22,17 @@ class DocWalker:
         queue.append((root, ()))
         while(queue):
             node, path = queue.popleft()
+            for child in ast.iter_child_nodes(node):
+                if isinstance(child, ast.ClassDef) or isinstance(child, ast.FunctionDef):
+                    new_path = path + (child.name, )
+                    yield ".".join(new_path)
+                    queue.append((child, new_path))
+
+    def _walk_depth_first(self, root):
+        queue = deque()
+        queue.append((root, ()))
+        while(queue):
+            node, path = queue.pop()
             for child in ast.iter_child_nodes(node):
                 if isinstance(child, ast.ClassDef) or isinstance(child, ast.FunctionDef):
                     new_path = path + (child.name, )
