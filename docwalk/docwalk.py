aa5ea712c38cd240aac9eb314f110be4a9990347
Johan Hägg <kaustic@gmx.co.uk>
2014-05-02 19:54:46 +0200

    Wrote iterative bredth-first traversal

--- docwalk/docwalk.py
+++ docwalk/docwalk.py
@@ -7,7 +7,7 @@ class DocWalker:
             self._root = ast.parse(f.read(), filename=filename)
 
     def run(self):
-        return list(self._walk_depth_first(self._root))
+        return list(self._walk_breadth_first(self._root))
 
     def _walk_depth_first(self, node, path=()):
         for child in ast.iter_child_nodes(node):
@@ -16,3 +16,14 @@ class DocWalker:
                 yield ".".join(new_path)
                 for path_name in self._walk_depth_first(child, new_path):
                     yield path_name
+
+    def _walk_breadth_first(self, root):
+        queue = deque()
+        queue.append((root, ()))
+        while(queue):
+            node, path = queue.popleft()
+            for child in ast.iter_child_nodes(node):
+                if isinstance(child, ast.ClassDef) or isinstance(child, ast.FunctionDef):
+                    new_path = path + (child.name, )
+                    yield ".".join(new_path)
+                    queue.append((child, new_path))
